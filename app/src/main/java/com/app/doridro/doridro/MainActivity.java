f0cd06d98d549f378acd62169b8ac07159212c58
Anisur Rahman Sagor <a7984834@gmail.com>
2018-08-12 22:41:00 +0600

    add button class

--- app/src/main/java/com/app/doridro/doridro/MainActivity.java
+++ app/src/main/java/com/app/doridro/doridro/MainActivity.java
@@ -6,9 +6,12 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button top;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
